69b7544ee482b534e27d8a63433d26e8170b80c1
VJL0 <jlvictor2020@gmail.com>
2025-01-04 20:25:10 -0500

    no cors everthing

--- src/pages/TestPage.tsx
+++ src/pages/TestPage.tsx
@@ -1,57 +1,22 @@
 const TestPage = () => {
   async function createSession() {
-    try {
-      // Send POST request to /proxy/session
-      const postResponse = await fetch(
-        "https://prd-xereg.temple.edu/StudentRegistrationSsb/ssb/term/search?mode=search",
-        {
-          method: "POST",
-          credentials: "include",
-          body: JSON.stringify({ term: 202436 }),
-          mode: "no-cors",
-        }
-      );
-
-      // Check if the response was successful
-      if (postResponse.ok) {
-        console.log("POST request successful");
-      } else {
-        console.log("Failed to create session, status:", postResponse.status);
+    await fetch(
+      "https://prd-xereg.temple.edu/StudentRegistrationSsb/ssb/term/search?mode=search",
+      {
+        method: "POST",
+        credentials: "include",
+        body: JSON.stringify({ term: 202436 }),
+        mode: "no-cors",
       }
-
-      // Log all cookies set by the server
-      const cookies = document.cookie; // Check cookies set in the browser
-      console.log("Current cookies:", cookies);
-
-      // Logging headers (Browser won't allow accessing set-cookie directly)
-      const allHeaders = postResponse.headers;
-      allHeaders.forEach((value, key) => {
-        console.log(key, value);
-      });
-    } catch (error) {
-      console.error("Error creating session:", error);
-    }
+    );
   }
 
   async function fetchCourse() {
-    try {
-      const getResponse = await fetch(
-        "https://tu-backend.onrender.com/testing",
-        {
-          method: "GET",
-          credentials: "include", // Ensure cookies are included in the request
-        }
-      );
-
-      if (getResponse.ok) {
-        const data = await getResponse.json();
-        console.log("Response data:", data);
-      } else {
-        console.log("Failed to fetch course data, status:", getResponse.status);
-      }
-    } catch (error) {
-      console.error("Error fetching course data:", error);
-    }
+    await fetch("https://tu-backend.onrender.com/testing", {
+      method: "GET",
+      credentials: "include", // Ensure cookies are included in the request
+      mode: "no-cors",
+    });
   }
 
   return (
